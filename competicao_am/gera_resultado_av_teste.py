ac79252d82a345891e83ccc1eac7ebdabc8850a6
Daniel Hasan <prof.daniel.hasan@gmail.com>
2020-10-08 13:02:46 -0300

    Primeiro codigo

---
+++ competicao_am/gera_resultado_av_teste.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from competicao_am.gerar_resultado_teste import gerar_saida_teste
+
+#altere aqui para o número correspondente ao seu grupo
+num_grupo = 11223344 
+
+#leia o dataset fornecido pelo professor (coloquei apenas um exemplo, na entrega, será outro)
+df_amostra_teste = pd.read_csv("/home/profhasan/git/aulas/machine-learning/datasets_competicoes/datasets/movies_amostra_teste_ex.csv")
+
+gerar_saida_teste(df_amostra_teste,"genero", num_grupo)
