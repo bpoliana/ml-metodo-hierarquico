673f8ee3dbca354973bf9ebabf7fbe3240bef760
Barbara Jaber <barbara.jaber@thoughtworks.com>
2020-11-09 22:30:19 -0300

    [WIP] Adiciona Metodo Tradicional para solução

--- competicao_am/avaliacao_competicao.py
+++ competicao_am/avaliacao_competicao.py
@@ -1,7 +1,7 @@
-from base_am.avaliacao import OtimizacaoObjetivo
+from base_am.avaliacao import OtimizacaoObjetivo, OtimizacaoObjetivoBOW, OtimizacaoObjetivoMetodoSimples
 from base_am.metodo import MetodoAprendizadoDeMaquina
 from base_am.resultado import Fold, Resultado
-from competicao_am.metodo_competicao import MetodoHierarquico
+from competicao_am.metodo_competicao import MetodoHierarquico, MetodoTradicional
 import optuna
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
@@ -24,6 +24,7 @@ class OtimizacaoObjetivoSVMCompeticao(OtimizacaoObjetivo):
 
         scikit_method = LinearSVC(C=2**exp_cost, random_state=2)
 
+        print("OtimizacaoObjetivoSVMCompeticao")
         return MetodoHierarquico(scikit_method, "genre")
 
     def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
@@ -77,6 +78,78 @@ class OtimizacaoObjetivoNaiveBayes(OtimizacaoObjetivo):
     def obtem_metodo(self,trial: optuna.Trial)->MetodoAprendizadoDeMaquina:
         scikit_method = GaussianNB()
         return MetodoHierarquico(scikit_method, "genre")
+
+    def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
+        return resultado.macro_f1
+
+
+class OtimizacaoObjetivoSVMTradicional(OtimizacaoObjetivo):
+    def __init__(self, fold:Fold):
+        super().__init__(fold)
+
+    def obtem_metodo(self,trial: optuna.Trial)->MetodoAprendizadoDeMaquina:
+        #Um custo adequado para custo pode variar muito, por ex, para uma tarefa 
+        #o valor de custo pode ser 10, para outra, 32000. 
+        #Assim, normalmente, para conseguir valores mais distintos,
+        #usamos c=2^exp_cost
+        exp_cost = trial.suggest_uniform('COST', -5, 15)
+
+        scikit_method = LinearSVC(C=2**exp_cost, random_state=2)
+
+        print("OtimizacaoObjetivoSVMCompeticao")
+        return MetodoTradicional(scikit_method, "original_genre")
+
+    def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
+        return resultado.macro_f1
+
+    
+class OtimizacaoObjetivoRFTradicional(OtimizacaoObjetivo):
+    def __init__(self, fold:Fold):
+        super().__init__(fold)
+
+    def obtem_metodo(self,trial: optuna.Trial)->MetodoAprendizadoDeMaquina:
+        #Um custo adequado para custo pode variar muito, por ex, para uma tarefa 
+        #o valor de custo pode ser 10, para outra, 32000. 
+        #Assim, normalmente, para conseguir valores mais distintos,
+        #usamos c=2^exp_cost
+        min_samples = trial.suggest_uniform('min_samples_split', 0.1, 0.5)
+        max_features = trial.suggest_uniform('max_features', 0.1, 0.9)
+        num_arvores = trial.suggest_int('num_arvores', 5, 100)
+        scikit_method = RandomForestClassifier(min_samples_split=min_samples,n_estimators=num_arvores,
+                                            max_features=max_features,random_state=2)
+
+        return MetodoTradicional(scikit_method, "original_genre")
+
+    def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
+        return resultado.macro_f1
+
+class OtimizacaoObjetivoKNNTradicional(OtimizacaoObjetivo):
+    
+    def __init__(self, fold:Fold):
+        super().__init__(fold)
+        
+
+    def obtem_metodo(self,trial: optuna.Trial)->MetodoAprendizadoDeMaquina:
+        #Um custo adequado para custo pode variar muito, por ex, para uma tarefa 
+        #o valor de custo pode ser 10, para outra, 32000. 
+        #Assim, normalmente, para conseguir valores mais distintos,
+        #usamos c=2^exp_cost
+        sample_size = 1000
+        n_neighbors = trial.suggest_int('n_neighbors', 0.1*sample_size, 0.4*sample_size)
+        scikit_method = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+        return MetodoTradicional(scikit_method, "original_genre")
+
+    def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
+        return resultado.macro_f1
+
+class OtimizacaoObjetivoNaiveBayesTradicional(OtimizacaoObjetivo):
+    def __init__(self, fold:Fold):
+        super().__init__(fold)
+
+    def obtem_metodo(self,trial: optuna.Trial)->MetodoAprendizadoDeMaquina:
+        scikit_method = GaussianNB()
+        return MetodoTradicional(scikit_method, "original_genre")
 
     def resultado_metrica_otimizacao(self,resultado: Resultado) -> float:
         return resultado.macro_f1
--- competicao_am/metodo_competicao.py
+++ competicao_am/metodo_competicao.py
@@ -69,16 +69,17 @@ class MetodoHierarquico(MetodoAprendizadoDeMaquina):
         if col_classe in df_data_to_predict.columns:
             x_to_predict = df_data_to_predict.drop(["index", col_classe, self.col_classe_prim_nivel], axis = 1)
         return x_treino, x_to_predict
-                                                                
+
+
     def eval(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str, seed:int=1):
         self.obj_class_final.class_to_number(df_treino[col_classe])
-        print(f"Mapeamento geral: {self.obj_class_final.dic_int_to_nom_classe}")
+        # print(f"Mapeamento geral: {self.obj_class_final.dic_int_to_nom_classe}")
         
         #################### Primeiro Nivel #################################                                                    
         #separação da classe 
         x_treino, x_to_predict = self.obtem_x(df_treino, df_data_to_predict, col_classe)
         y_treino, y_to_predict = self.obtem_y(df_treino, df_data_to_predict, col_classe, True)
-        #print(f"x_treino: {y_treino} y_to_predict:{y_to_predict}")
+        # print(f"x_treino: {y_treino} y_to_predict:{y_to_predict}")
         # print(self.obj_class_prim_nivel.dic_int_to_nom_classe)
         is_bow = "is_bow" in df_treino
         if is_bow:
@@ -129,22 +130,105 @@ class MetodoHierarquico(MetodoAprendizadoDeMaquina):
                     val_predict = arr_predict_prim_nivel[pos_original]
                     final_classe_nome = self.obj_class_prim_nivel.dic_int_to_nom_classe[val_predict]
                     arr_predict_final[pos_original] = self.obj_class_final.dic_nom_classe_to_int[final_classe_nome]
-        
+        # print(y_to_predict, arr_predict_final)
+        # print("jasdiajsaiajdisajidjsa")
         return Resultado(y_to_predict, arr_predict_final)
 
     
-class MetodoSimples(MetodoAprendizadoDeMaquina):
-    def eval(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str, seed:int=1):
+class MetodoTradicional(MetodoAprendizadoDeMaquina):
+#você pode mudar a assinatura desta classe (por exemplo, usar dois metodos e o resultado da predição
+    # seria a combinação desses dois)
+    def __init__(self,ml_method:Union[ClassifierMixin,RegressorMixin],  col_classe_prim_nivel=""):
+        #caso fosse vários métodos, não há problema algum passar um array de todos os métodos como parametro ;)
+        self.ml_method = ml_method
+        self.obj_class_final = ClasseNumerica()
+        self.col_classe_prim_nivel = col_classe_prim_nivel
+
+        self.obj_class_prim_nivel = ClasseNumerica()
+        #mapeamento int=>classe e classe=>int a ser usado
+        self.dic_int_to_nom_classe = {}
+        self.dic_nom_classe_to_int = {}
+
+    def class_to_number(self,y):
+        arr_int_y = []
+
+        #mapeia cada classe para um número
+        for rotulo_classe in y:
+            #cria um número para esse rotulo de classe, caso não exista ainda
+            if rotulo_classe not in self.dic_nom_classe_to_int:
+                int_new_val_classe = len(self.dic_nom_classe_to_int.keys())
+                self.dic_nom_classe_to_int[rotulo_classe] = int_new_val_classe
+                self.dic_int_to_nom_classe[int_new_val_classe] = rotulo_classe
+
+            #adiciona esse item
+            arr_int_y.append(self.dic_nom_classe_to_int[rotulo_classe])
+
+        return arr_int_y
+    def obtem_y(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str, prim_nivel:bool):
+        
+        col_y_atual = self.col_classe_prim_nivel if prim_nivel else  col_classe
+        #converte no treino e teste
+
+        y_treino = self.obj_class_prim_nivel.class_to_number(df_treino[col_y_atual])
+        y_to_predict = self.obj_class_prim_nivel.class_to_number(df_data_to_predict[col_y_atual])
+        
+        return y_treino,y_to_predict
+
+    def obtem_x(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str):
+        
+        x_treino = df_treino.drop(["index", col_classe, self.col_classe_prim_nivel] , axis = 1)
+        x_to_predict = df_data_to_predict
+        if col_classe in df_data_to_predict.columns:
+            x_to_predict = df_data_to_predict.drop(["index", col_classe, self.col_classe_prim_nivel], axis = 1)
+        return x_treino, x_to_predict
+
+    def eval_bow(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str, seed:int=1):
         #separação da classe 
         x_treino, x_to_predict = self.obtem_x(df_treino, df_data_to_predict, col_classe)
         y_treino, y_to_predict = self.obtem_y(df_treino, df_data_to_predict, col_classe)
+        
 
         #geração dos atributos por meio do df_treino e df_data_to_predict
         df_treino_bow, df_to_predict_bow = gerar_atributos_lyrics(x_treino, x_to_predict)
 
+
+        #treina e aplica os modelos de cada representação
+        #o meotod fit altera o proprio objeto `ml_method`, por isso, temos que fazer um fit e depois
+        # seu respectivo predict  
         self.ml_method.fit(df_treino_bow, y_treino)
         arr_predict = self.ml_method.predict(df_to_predict_bow)
 
         #if y_to_predict:
         #   print(classification_report(y_to_predict, arr_predict))
         return y_to_predict, arr_predict
+    def combine_predictions(self, arr_predictions_1:List[int], arr_predictions_2:List[int]) -> List[int]:
+        #realiza a predicao final
+        #.. isso é apenas um exemplo de combinação, sem nenhuma justificativa do por que optei por isso
+        #... Porém, você pode analisar os resultados de cada representação independentemente e pensar
+        #... em uma heuristica para a combinação
+        #sinta-se livre de mudar também a assinatura desse método - caso queira combinar 3 (metodos e/ou representações)
+
+        y_final_predictions = []
+        for i,pred in enumerate(arr_predictions_1):
+            if self.dic_int_to_nom_classe[pred] == 'Comedy':
+                y_final_predictions.append(pred)
+            else: 
+                y_final_predictions.append(arr_predictions_2[i])
+        return y_final_predictions
+
+    def eval(self, df_treino:pd.DataFrame, df_data_to_predict:pd.DataFrame, col_classe:str, seed:int=1):
+        self.obj_class_final.class_to_number(df_treino[col_classe])
+
+        x_treino, x_to_predict = self.obtem_x(df_treino, df_data_to_predict, col_classe)
+        y_treino, y_to_predict = self.obtem_y(df_treino, df_data_to_predict, col_classe, True)
+      
+       # is_bow = "is_bow" in df_treino
+        # if is_bow:
+        #     y_to_predict, arr_predictions_bow = self.eval_bow(df_treino, df_data_to_predict, col_classe)
+        
+        self.ml_method.fit(x_treino, y_treino)
+        arr_predict = self.ml_method.predict(x_to_predict)
+
+        #if y_to_predict:
+        #   print(classification_report(y_to_predict, arr_predict))
+        return Resultado(y_to_predict, arr_predict)
